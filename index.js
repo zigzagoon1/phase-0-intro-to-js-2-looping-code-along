2c3b30f7d53a176c3fcecb0519d0dd4b68dd8749
KellyMark <kmark602@gmail.com>
2022-11-27 19:49:36 -0500

    commit tree b085335427923a8aae4cee2727da52f2f3e731c2

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solutions in this file
+// for (let age = 30; age < 40; age++) {
+//     console.log(`I'm ${age} years old. Happy birthday to me!`);
+//   }
+
+// const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+  }
+
+  return gifts;
+}
+
+//wrapGifts(gifts);
+
+function writeCards(names, eventName) {
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+        const message = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+        messages.push(message);
+    }
+    return messages;
+}
+
+//console.log(writeCards(["Charlie", "Samip", "Ali"], "birthday"));
+
+function countDown(number) {
+    let i = number;
+    while (i >= 0){
+        console.log(i);
+        i--;
+    }
+}
+countDown(10);
